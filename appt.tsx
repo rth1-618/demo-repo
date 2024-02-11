6073d3abd5a997373c3ba3b646a8cc9fff92252c
rth1-618 <rth1.618@gmail.com>
2024-02-11 22:57:24 +0530

    noname added

--- appt.tsx
+++ appt.tsx
@@ -3,11 +3,12 @@
 const appt = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut accusamus
-      mollitia dolor dolores officia quod aperiam omnis, necessitatibus impedit
-      ipsum, neque qui voluptate sunt porro maiores? Numquam ipsa repellendus
-      maiores consequuntur sunt unde quia. Ea, hic repellat. Beatae soluta
-      dolore enim, obcaecati iste minus rerum nulla amet numquam ab optio.
+      NO NAME Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+      accusamus mollitia dolor dolores officia quod aperiam omnis,
+      necessitatibus impedit ipsum, neque qui voluptate sunt porro maiores?
+      Numquam ipsa repellendus maiores consequuntur sunt unde quia. Ea, hic
+      repellat. Beatae soluta dolore enim, obcaecati iste minus rerum nulla amet
+      numquam ab optio.
     </div>
   );
 };
